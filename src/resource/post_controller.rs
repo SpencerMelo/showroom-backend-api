096c83faac00156d07fe8a85821cedb0c5bcbb99
Spencer Melo <su.spencer@hotmail.com>
2024-02-10 00:39:26 -0300

    move post database access to service layer

--- src/resource/post_controller.rs
+++ src/resource/post_controller.rs
@@ -3,14 +3,14 @@ use axum::extract::{Path, State};
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 use axum::routing::{get, post, patch};
-use diesel::{ExpressionMethods, PgConnection, QueryDsl, RunQueryDsl, SelectableHelper};
+use diesel::PgConnection;
 use diesel::r2d2::{ConnectionManager, Pool};
-use diesel::prelude::*;
 use uuid::Uuid;
 
 use showroom_backend_api::models::models::Post;
 use showroom_backend_api::models::models::CreatePost;
-use showroom_backend_api::schema::posts::dsl::*;
+
+use crate::service::post_service;
 
 pub fn router(pool: Pool<ConnectionManager<PgConnection>>) -> Router {
     Router::new()
@@ -22,71 +22,29 @@ pub fn router(pool: Pool<ConnectionManager<PgConnection>>) -> Router {
 }
 
 pub async fn get_all(State(pool): State<Pool<ConnectionManager<PgConnection>>>) -> Response {
-    let connection: &mut PgConnection = &mut pool.get().unwrap();
-
-    let results: Vec<Post> = posts
-        .filter(published.eq(true))
-        .limit(20)
-        .select(Post::as_select())
-        .load(connection)
-        .expect("Error loading posts");
-
+    let results: Vec<Post> = post_service::get_all(pool, 100);
     (StatusCode::OK, Json(results)).into_response()
 }
 
 pub async fn get_one(State(pool): State<Pool<ConnectionManager<PgConnection>>>, Path(post_id): Path<Uuid>) -> Response {
-    let connection: &mut PgConnection = &mut pool.get().unwrap();
-
-    let result = posts
-        .filter(id.eq(post_id))
-        .select(Post::as_select())
-        .first(connection)
-        .optional();
-
+    let result: Option<Post> = post_service::get_post(pool, post_id);
     match result {
-        Ok(Some(result)) => (StatusCode::OK, Json(result)).into_response(),
-        Ok(None) | Err(_) => (StatusCode::NOT_FOUND, String::from("")).into_response(),
+        Some(result) => (StatusCode::OK, Json(result)).into_response(),
+        None => StatusCode::NOT_FOUND.into_response(),
     }
 }
 
 pub async fn create(State(pool): State<Pool<ConnectionManager<PgConnection>>>, Json(payload): Json<CreatePost>) -> Response {
-    let connection: &mut PgConnection = &mut pool.get().unwrap();
-
-    let new_post: Post = Post {
-        id: Uuid::new_v4(),
-        brand: payload.brand,
-        model: payload.model,
-        version: payload.version,
-        engine: payload.engine,
-        transmission: payload.transmission,
-        year: payload.year,
-        mileage: payload.mileage,
-        color: payload.color,
-        body: payload.body,
-        armored: payload.armored,
-        exchange: payload.exchange,
-        price: payload.price,
-        thumbnail_url: payload.thumbnail_url,
-        author: payload.author,
-        published: true,
-    };
-
-    let result: Post = diesel::insert_into(posts)
-        .values(&new_post)
-        .returning(Post::as_returning())
-        .get_result(connection).expect("Error saving new post");
-
+    let result = post_service::create_post(pool, payload);
     (StatusCode::CREATED, Json(result)).into_response()
 }
 
 pub async fn update(State(pool): State<Pool<ConnectionManager<PgConnection>>>, Json(payload): Json<Post>) -> Response {
-    let connection: &mut PgConnection = &mut pool.get().unwrap();
+    let affect_count = post_service::update_post(pool, payload);
 
-    diesel::update(posts)
-        .filter(id.eq(payload.id))
-        .set(payload)
-        .execute(connection)
-        .expect("Error patching post");
-
-    StatusCode::NO_CONTENT.into_response()
+    if affect_count > 0 {
+        StatusCode::NO_CONTENT.into_response()
+    } else {
+        StatusCode::NOT_FOUND.into_response()
+    }
 }
---
+++ src/service/post_service.rs
@@ -0,0 +1,79 @@
+use showroom_backend_api::models::models::{CreatePost, Post};
+use showroom_backend_api::schema::posts::dsl::*;
+
+use diesel::prelude::*;
+use diesel::r2d2::{ConnectionManager, Pool};
+use diesel::{ExpressionMethods, PgConnection, QueryDsl, RunQueryDsl, SelectableHelper};
+
+use uuid::Uuid;
+
+pub fn get_all(pool: Pool<ConnectionManager<PgConnection>>, limit: u8) -> Vec<Post> {
+    let connection: &mut PgConnection = &mut pool.get().unwrap();
+
+    posts
+        .filter(published.eq(true))
+        .limit(limit.into())
+        .select(Post::as_select())
+        .load(connection)
+        .expect("Error loading posts")
+}
+
+pub fn get_post(pool: Pool<ConnectionManager<PgConnection>>, post_id: Uuid) -> Option<Post> {
+    let connection: &mut PgConnection = &mut pool.get().unwrap();
+
+    let result = posts
+        .filter(id.eq(post_id))
+        .select(Post::as_select())
+        .first(connection)
+        .optional();
+
+    match result {
+        Ok(result) => result,
+        Err(_) => return None,
+    }
+}
+
+pub fn create_post(pool: Pool<ConnectionManager<PgConnection>>, create_post: CreatePost) -> Post {
+    let connection: &mut PgConnection = &mut pool.get().unwrap();
+
+    let new_post: Post = Post {
+        id: Uuid::new_v4(),
+        brand: create_post.brand,
+        model: create_post.model,
+        version: create_post.version,
+        engine: create_post.engine,
+        transmission: create_post.transmission,
+        year: create_post.year,
+        mileage: create_post.mileage,
+        color: create_post.color,
+        body: create_post.body,
+        armored: create_post.armored,
+        exchange: create_post.exchange,
+        price: create_post.price,
+        thumbnail_url: create_post.thumbnail_url,
+        author: create_post.author,
+        published: true,
+    };
+
+    let result: Post = diesel::insert_into(posts)
+        .values(&new_post)
+        .returning(Post::as_returning())
+        .get_result(connection)
+        .expect("Error saving new post");
+
+    result
+}
+
+pub fn update_post(pool: Pool<ConnectionManager<PgConnection>>, post: Post) -> usize {
+    let connection: &mut PgConnection = &mut pool.get().unwrap();
+
+    let update_count = diesel::update(posts)
+        .filter(id.eq(post.id))
+        .set(post)
+        .execute(connection);
+
+    match update_count {
+        Ok(update_count) => update_count,
+        Err(_) => 0
+    }
+}
