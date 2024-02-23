7f8619558500af8d88ac70fcf2080b17157e4215
Spencer Melo <su.spencer@hotmail.com>
2024-02-23 19:37:11 -0300

    add test for variables loaded from environment

--- src/database/database.rs
+++ src/database/database.rs
@@ -40,3 +40,77 @@ fn get_connection_timeout() -> Duration {
             });
     Duration::from_secs(timeout)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serial_test::serial;
+
+    #[test]
+    #[serial(timeout)]
+    fn no_timeout_on_env() {
+        // Given 'there is no timeout in environment variable'
+        env::remove_var("DATABASE_CONNECTION_TIMEOUT");
+
+        // When 'the application get the connection timeout'
+        let current_timeout: Duration = get_connection_timeout();
+
+        // Then 'the timeout should be the default'
+        assert_eq!(current_timeout, Duration::from_secs(30));
+    }
+
+    #[test]
+    #[serial(timeout)]
+    fn invalid_timeout_on_env() {
+        // Given 'there is a invalid timeout in environment variable'
+        env::remove_var("DATABASE_CONNECTION_TIMEOUT");
+        env::set_var("DATABASE_CONNECTION_TIMEOUT", "F");
+
+        // When 'the application get the connection timeout'
+        let current_timeout: Duration = get_connection_timeout();
+
+        // Then 'the timeout should be the default'
+        assert_eq!(current_timeout, Duration::from_secs(30));
+    }
+
+    #[test]
+    #[serial(timeout)]
+    fn valid_timeout_on_env() {
+        // Given 'there is a valid timeout in environment variable'
+        env::remove_var("DATABASE_CONNECTION_TIMEOUT");
+        env::set_var("DATABASE_CONNECTION_TIMEOUT", "10");
+
+        // When 'the application get the connection timeout'
+        let current_timeout: Duration = get_connection_timeout();
+
+        // Then 'the timeout should be the value from the environment variable'
+        assert_eq!(current_timeout, Duration::from_secs(10));
+    }
+
+    #[test]
+    #[serial(db_url)]
+    fn no_database_url_on_env() {
+        // Given 'there is not a database url in environment variable'
+        env::remove_var("DATABASE_URL");
+
+        // When 'the application get the database url'
+        let current_database_url = read_database_url();
+
+        // Then 'the result should be an Error'
+        assert_eq!(current_database_url, Err(env::VarError::NotPresent));
+    }
+
+    #[test]
+    #[serial(db_url)]
+    fn database_url_on_env() {
+        // Given 'there is a database url in environment variable'
+        env::remove_var("DATABASE_URL");
+        env::set_var("DATABASE_URL", "current_value");
+
+        // When 'the application get the database url'
+        let current_database_url = read_database_url();
+
+        // Then 'the database url should be the value from the environment variable'
+        assert_eq!(current_database_url.unwrap(), "current_value");
+    }
+}
